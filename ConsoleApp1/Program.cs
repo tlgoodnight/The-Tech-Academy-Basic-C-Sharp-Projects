0e3ee492bd345825b23862bc3c07efb6aabf7ff7
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-02-16 13:33:20 -0800

    ConsoleApp1
    Console App submission

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+     }
+}
+
--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 <h3>Introduction</h3>
  
   This repository will be for The Tech Academy Basic C Sharp projects that I complete.
+
+	<h5>ConsoleApp1<h5>
+		This is a basic template and generic app that uses the Console. 
+
  
   
   
