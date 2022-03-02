e6e91eccc64616de038f60bf046868aa32800d35
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-02 10:46:45 -0800

    Resubmit Challenge

--- CallingMethodsChallenge2/Program.cs
+++ CallingMethodsChallenge2/Program.cs
@@ -10,20 +10,27 @@ namespace CallingMethodsChallenge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many hours a day do you sleep?");
-            int result1 = MethodsClass.Method1(Convert.ToInt32(Console.ReadLine()));//calls class and method. 
-            Console.WriteLine("You sleep approximately " + result1 + " hours per year.");
-            Console.ReadLine();
-
-            Console.WriteLine("How much is a gallon of gas in your area?");
-            decimal result2 = MethodsClass.Method1(Convert.ToDecimal(Console.ReadLine()));//calls class and method
-            Console.WriteLine("It costs approximately $" + result2 + " to fill up your tank.");
-            Console.ReadLine();
-
-            Console.WriteLine("How many hours week do you work?");
-            string result3 = MethodsClass.Method1(Console.ReadLine());//calls class and method
-            Console.WriteLine("You work approximately " + result3 + " total days in a year");
-            Console.ReadLine();
+            {
+                MethodsClass methods = new MethodsClass();
+                Console.WriteLine("How many hours a day do you sleep?");
+                int result1 = methods.Method1(Convert.ToInt32(Console.ReadLine()));//calls class and method. 
+                Console.WriteLine("You sleep approximately " + result1 + " hours per year.");
+                Console.ReadLine();
+            }
+            {
+                MethodsClass methods = new MethodsClass();
+                Console.WriteLine("How much is a gallon of gas in your area?");
+                decimal result2 = methods.Method1(Convert.ToDecimal(Console.ReadLine()));//calls class and method
+                Console.WriteLine("It costs approximately $" + result2 + " to fill up your tank.");
+                Console.ReadLine();
+            }
+            {
+                MethodsClass methods = new MethodsClass();
+                Console.WriteLine("How many hours week do you work?");
+                string result3 = methods.Method1(Console.ReadLine());//calls class and method
+                Console.WriteLine("You work approximately " + result3 + " total days in a year");
+                Console.ReadLine();
+            }
         }
     }
 }
