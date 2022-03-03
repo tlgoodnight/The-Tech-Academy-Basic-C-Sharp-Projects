7bf6f7f9e90bb9c35f11bde0b37429cd70315a87
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-03 14:45:03 -0800

    Method Challenge 4

---
+++ CallingMethodsChallenge4/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+class TestVoid
+{
+   
+    public void result(int squarenmb, int solonmbr)
+    {
+        int sqrroot = squarenmb * squarenmb;
+        Console.WriteLine("The square root of {0} is : " + sqrroot + ". \n And the second integer not used in the math operation is {0}", squarenmb,  solonmbr);
+    }
+}
---
+++ CallingMethodsChallenge4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TestVoid t = new TestVoid();
+            t.result(5, 10);
+            Console.ReadLine();
+        
+            
+            t.result(squarenmb:5,solonmbr:10);
+            Console.ReadLine();
+        }
+    }
+    
+}
