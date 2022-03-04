f4910424a5245f9ea854d5b97c0a36505b690f09
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-04 14:08:19 -0800

    PolymorphismAssignment

---
+++ MethodsChallenge8Polymorphism/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Employee : Person, IQuitable
+{
+    public int Id { get; set; }
+
+    public void Quit()
+    {
+        Console.WriteLine("Don't let the door hit you.");
+    }
+
+    public override void SayName()
+    {
+        Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+    }
+    
+
+}
+
---
+++ MethodsChallenge8Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsChallenge6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            
+
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            employee.SayName();
+            Console.ReadLine();
+
+            IQuitable quitable = new Employee();
+            quitable.Quit();
+            Console.ReadLine();
+        }
+    }
+}
