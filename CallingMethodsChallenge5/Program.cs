ddf36b5b867c80a3dcbcb7beceb9d827fa12bf43
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-03 16:19:51 -0800

    Methods Challenge 5

---
+++ CallingMethodsChallenge5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2 instantiate that class
+            TestVoid t = new TestVoid();
+            {
+                //have user enter a number call the the method display the output
+                Console.WriteLine("Please pick a number to divide by 2: ");
+                int int1 = Convert.ToInt32(Console.ReadLine());
+                t.result(int1);
+                Console.ReadLine();
+                //this is an overload method for above.
+                Console.WriteLine("Please pick a number to divide by 2: ");
+                int int2 = Convert.ToInt32(Console.ReadLine());
+                t.result(int2);
+                Console.ReadLine();
+
+                //this is the format for an out argument example - 
+                int initializeInMethod;
+                t.OutArgExample(out initializeInMethod);
+                Console.WriteLine(initializeInMethod);// results = 44 
+                Console.ReadLine();
+            }
+            
+            {
+                //this code sets the static class Family and field(Name) to Goodnight
+                Family.Name = "Goodnight";
+                Console.WriteLine(Family.Name);
+                Console.ReadLine();
+            }
+        }
+    }
+    
+}
---
+++ CallingMethodsChallenge5/Properties/Class2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Family
+{
+    private static string name;
+    public static string Name { get => name; set => name = value; }
+}
+
+
