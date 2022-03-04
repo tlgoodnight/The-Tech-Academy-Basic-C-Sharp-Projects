6d3f6db861f1f8e1cdd026497b299c53db4b90f7
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-04 11:02:28 -0800

    AbstractClassChallenge

---
+++ MethodsChallenge7/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public abstract class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+     public virtual void SayName()
+    {
+         Console.WriteLine("Name: {0} {1}", FirstName, LastName);
+    }
+}        
