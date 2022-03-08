cda889bde9f62cc466b7a616aca8cf0eb19b313f
tlgoodnight <91025564+tlgoodnight@users.noreply.github.com>
2022-03-08 09:56:41 -0800

    TwentyOne

---
+++ TwentyOneApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOneApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////List<Game> games = new List<Game>();
+            //TwentyOneGame game = new TwentyOneGame();
+            ////Games.Add(game);
+            //game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            //game.ListPlayers();
+            //Console.ReadLine();
+
+
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
